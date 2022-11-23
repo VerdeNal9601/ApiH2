f976e2106e479d63a36fdd5cf25fe88d1b269991
juan sebastian <juansebastianalvareztabares@gmail.com>
2022-11-23 11:39:50 -0500

    añadi documentacion swagger

--- main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users(name, last_Name) VALUES('Luis', 'Perez');
-INSERT INTO users(name, last_Name) VALUES('Felipe', 'Calvache');
-INSERT INTO users(name, last_Name) VALUES('Rolando', 'Perez');
